33e309535cc0d26faa6c0218e34af4190ace774e
Jan Haslik <janhaslik@Air-von-Jan.fritz.box>
2024-04-21 17:34:19 +0200

    add tests and goroutines for crawling

--- crawler/main.go
+++ crawler/main.go
@@ -1,8 +1,17 @@
 package main
 
-func main() {
-	crawl("https://apple.com")
-	go func() {
+import (
+	"fmt"
+	"web-crawler/crawler"
+)
 
-	}()
+func main() {
+	urls := []string{"https://apple.com", "https://www.kapsch.net", "https://www.microsoft.com", "https://www.google.com", "https://www.amazon.de"}
+
+	numWorkers := 1 //number of workers, prod: 1 for 10 urls, test: 1 for 1 url
+	err := crawler.Crawl(urls, numWorkers)
+	if err != nil {
+		fmt.Println("Error in main at crawler:", err)
+		return
+	}
 }
---
+++ crawler/tests/crawler_test.go
@@ -0,0 +1,51 @@
+package tests
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"web-crawler/crawler"
+)
+
+func TestCleanContent(t *testing.T) {
+	input := "This is a: test, of \"the\" 'cleaning' function."
+	expected := "this is a test of the cleaning function"
+
+	cleaned := crawler.CleanContent(input)
+
+	if cleaned != expected {
+		t.Errorf("cleanContent did not produce the expected result. Got: %s, Want: %s", cleaned, expected)
+	}
+}
+
+func TestExtractStrings(t *testing.T) {
+	input := "Line 1\n\n  Line 2\nLine 3\n"
+	expected := "Line 1 Line 2 Line 3"
+
+	extracted := crawler.ExtractStrings(input)
+
+	if extracted != expected {
+		t.Errorf("extractStrings did not produce the expected result. Got: %s, Want: %s", extracted, expected)
+	}
+}
+
+func TestCrawlAndFetch(t *testing.T) {
+	// start a local http server to handle requests during testing
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		_, err := w.Write([]byte("whats good"))
+		if err != nil {
+			t.Errorf("response writer did not work. Got: %s", err)
+		}
+	}))
+	defer server.Close()
+
+	err := crawler.Crawl([]string{"dasdasd"}, 1)
+	if err == nil {
+		t.Errorf("crawl did not return an error")
+	}
+
+	err = crawler.Crawl([]string{server.URL}, 1)
+	if err != nil {
+		t.Fail()
+	}
+}
