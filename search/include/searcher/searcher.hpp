be41c2161149e8d6df7ec81e45303280e54db8bf
Jan Haslik <janhaslik@Air-von-Jan.fritz.box>
2024-05-09 15:50:46 +0200

    add search function

--- search/include/searcher/searcher.hpp
+++ search/include/searcher/searcher.hpp
@@ -3,12 +3,14 @@
 #include <string>
 #include <vector>
 #include <db/db.hpp>
+#include <algorithm>
 
 namespace searcher{
 
     struct DocumentScores{
         float td_idf=0;
         float bm25=0;
+        float totalScore;
     };
 
     class Searcher{
@@ -19,10 +21,11 @@ namespace searcher{
             //key: url; value: scores
             std::unordered_map<std::string, DocumentScores> rank;
             std::shared_ptr<searcher_db::SearcherDB> db;
-            void splitQuery(const std::string& str, std::vector<std::string> segments, char delimiter);
+            void splitQuery(const std::string& str, std::vector<std::string>& segments, char delimiter);
             float combineScores(float td_idf, float bm25);
             float calculateIDF_Score(int totalDocuments, int appearences);
             float calculateBM25_Score(int doc_length, int avg_doc_length,float idf, float tf, float k1, float b);
+            static bool cmp(const std::pair<std::string, DocumentScores>& a, const std::pair<std::string, DocumentScores>& b);
     };
 }
 
--- search/searcher/searcher.cpp
+++ search/searcher/searcher.cpp
@@ -10,35 +10,53 @@ namespace searcher{
     Searcher::Searcher(){
         this->db=std::make_shared<searcher_db::SearcherDB>();
     }
-    
+
+    bool Searcher::cmp(const std::pair<std::string, DocumentScores>& a, const std::pair<std::string, DocumentScores>& b){
+        return a.second.totalScore>b.second.totalScore;
+    }
+
     std::vector<std::string> Searcher::search(std::string query){
         std::vector<std::string> querySegments;
         this->splitQuery(query,querySegments,'+');
 
         int totalDocuments=this->db->getTotalNumberDocuments();
 
+        //calculate the score for relevant documents
         for(std::string& term: querySegments){
             std::vector<searcher_db::IndexDocument> documents = this->db->getDocumentsByTerm(term);
-            float idf=calculateIDF_Score(totalDocuments,documents.size());
 
             //tuning params for bm25
-            float k1 = 1.2;            
+            float k1 = 1.2;//{1.2, 2.0}      
             float b = 0.75;
 
+            //calculate total idf
+            float idf=calculateIDF_Score(totalDocuments,documents.size());
+
             for(auto doc: documents){
-            
                 float td_idf=doc.tf*idf;
                 float bm25=calculateBM25_Score(doc.docLength, 0, idf, doc.tf, k1, b);
  
                 this->rank[doc.url].td_idf+=td_idf;
                 this->rank[doc.url].bm25+=bm25;
+                this->rank[doc.url].totalScore=combineScores(this->rank[doc.url].td_idf, this->rank[doc.url].bm25);
             }
         }
-        std::vector<std::string> resultUrls={"https://apple.com","https://microsoft.com"};
+
+        std::vector<std::pair<std::string, DocumentScores>> sorted_documents(rank.begin(), rank.end());
+        std::sort(sorted_documents.begin(), sorted_documents.end(), cmp);
+
+        std::vector<std::string> resultUrls;
+
+        int counter=0;
+        for(const auto& it: sorted_documents){
+            if(counter>25)break;
+            std::cout << it.first << " " << it.second.totalScore << " " << it.second.td_idf << " " << it.second.bm25 << " " << std::endl;
+            resultUrls.push_back(it.first);
+        }
         return resultUrls;
     }
 
-    void Searcher::splitQuery(const std::string& str, std::vector<std::string> segments, char delimiter){
+    void Searcher::splitQuery(const std::string& str, std::vector<std::string>& segments, char delimiter){
         std::istringstream isstream(str);
         std::string segment;
         while (std::getline(isstream, segment, delimiter)) {
@@ -55,6 +73,7 @@ namespace searcher{
     }
 
     float Searcher::combineScores(float td_idf, float bm25){
+        //weights for the ranking
         float weightTD_IDF=0.3;
         float weightBM25=0.7;
 
