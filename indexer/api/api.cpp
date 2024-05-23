4a62e5c159033d6c5989612667e2686f8245b8aa
Jan Haslik <janhaslik@Air-von-Jan.innolab.spengergasse.at>
2024-05-23 13:13:11 +0200

    add main class docs

--- indexer/api/api.cpp
+++ indexer/api/api.cpp
@@ -8,37 +8,54 @@
 #include <string>
 #include <memory>
 
+/**
+ * @brief Main function to run the indexing server.
+ * 
+ * This function sets up a router to handle HTTP POST requests for indexing documents.
+ * It initializes the MongoDB instance, sets up the endpoint, and starts the server.
+ * 
+ * @return int Returns 0 on successful execution, 1 on failure.
+ */
 int main() {
 
     jetpp::Router router;
-    
-    //call mongocxx instance once
+
+    // Call mongocxx instance once to initialize MongoDB driver
     mongocxx::instance instance{};
 
+    /**
+     * @brief Endpoint to handle document indexing requests.
+     * 
+     * This endpoint receives a document in JSON format via a POST request,
+     * extracts the URL and content, initializes the indexer, and indexes the document.
+     */
     router.post("/index", [&](jetpp::Request& req, jetpp::Response& res) {
         try {
+            // Get the document from the request body
             std::string document = req.body;
 
+            // Convert the document string to a JSON object
             jetpp::JsonConverter jsonConverter;
             jetpp::JsonValue doc = jsonConverter.stringToJson(document);
 
-            // access url and content from the JSON document
+            // Access URL and content from the JSON document
             std::string url = doc.asObject["url"].asString;
             std::string content = doc.asObject["content"].asString;
 
-            // init MongoDB indexer
+            // Initialize MongoDB indexer
             std::shared_ptr<indexer::Indexer> indexPtr;
             try {
                 indexPtr = std::make_shared<indexer::Indexer>();
-            }catch (const std::exception& e) {
+            } catch (const std::exception& e) {
                 std::cerr << "Error initializing indexer: " << e.what() << std::endl;
             }
 
-            // create an indexing document
+            // Create an indexing document
             indexer::Document indexingDocument{url, content};
-            // index the document
+            // Index the document
             indexPtr->indexDocument(&indexingDocument);
 
+            // Send success response
             res.status(200).send("Processing successful");
         } catch (const std::exception& e) {
             std::cerr << "Error processing indexing request: " << e.what() << std::endl;
@@ -46,7 +63,7 @@ int main() {
         }
     });
 
-    // start jet++ server on port 7001
+    // Start Jet++ server on port 7001
     jetpp::Server server(router);
     try {
         server.start(7001);
