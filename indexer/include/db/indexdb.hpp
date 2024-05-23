02bf91614011061b4100f6f2cba3d8a62417afd3
Jan Haslik <janhaslik@Air-von-Jan.innolab.spengergasse.at>
2024-05-23 13:07:05 +0200

    add indexer doc

--- indexer/include/db/indexdb.hpp
+++ indexer/include/db/indexdb.hpp
@@ -11,22 +11,60 @@
 
 namespace indexer_db {
 
+    /**
+     * @struct IndexDocument
+     * @brief Structure to represent an indexed document.
+     * 
+     * This structure holds the URL of the document, its term frequency (TF) for a specific term,
+     * and the length of the document.
+     */
     struct IndexDocument {
-        std::string url;
-        float tf;
-        int docLength;
+        std::string url;    ///< URL of the document.
+        float tf;           ///< Term Frequency of the specific term in the document.
+        int docLength;      ///< Length of the document.
     };
 
+    /**
+     * @class IndexerDB
+     * @brief A class to interact with the index database.
+     * 
+     * This class provides functionality to upsert (update or insert) documents into the index
+     * and retrieve documents for a specific term.
+     */
     class IndexerDB {
     public:
+        /**
+         * @brief Default constructor for the IndexerDB class.
+         * 
+         * Initializes the MongoDB client connection.
+         */
         IndexerDB();
+
+        /**
+         * @brief Destructor for the IndexerDB class.
+         * 
+         * Cleans up resources.
+         */
         ~IndexerDB();
 
+        /**
+         * @brief Upserts (updates or inserts) an index document for a specific term.
+         * 
+         * @param document The document to be indexed.
+         * @param term The term for which the document is being indexed.
+         */
         void upsertIndexDocument(const IndexDocument& document, std::string term);
-    private:
-        std::shared_ptr<mongocxx::client> client; // using shared_ptr for managing the database pointer
-        std::vector<IndexDocument> getTermDocuments(std::string term);
 
+    private:
+        std::shared_ptr<mongocxx::client> client; ///< Shared pointer to the MongoDB client.
+
+        /**
+         * @brief Retrieves documents that contain a specific term from the database.
+         * 
+         * @param term The search term.
+         * @return A vector of IndexDocument structures containing documents that include the term.
+         */
+        std::vector<IndexDocument> getTermDocuments(std::string term);
     };
 
 }
--- indexer/indexer/indexer.cpp
+++ indexer/indexer/indexer.cpp
@@ -5,32 +5,53 @@
 
 namespace indexer {
 
+    /**
+     * @brief Default constructor for the Indexer class.
+     * 
+     * Initializes the database connection using a shared pointer to IndexerDB.
+     */
     Indexer::Indexer() {
-        this->db=std::make_shared<indexer_db::IndexerDB>();
+        this->db = std::make_shared<indexer_db::IndexerDB>();
     }
 
+    /**
+     * @brief Indexes a given document.
+     * 
+     * This function processes the content of the document, extracts unique terms, calculates their term frequencies (TF),
+     * and inserts or updates the index database with the term information.
+     * 
+     * @param document Pointer to the document to be indexed.
+     */
     void Indexer::indexDocument(Document* document) {
         std::unordered_map<std::string, int> terms;
         splitContentUniqueTerms(document->content, terms, ' ');
 
-        // process each term extracted from the document
+        // Process each term extracted from the document
         for (const auto& pair : terms) {
             std::string term = pair.first;
             removeWhitespace(term);
 
-            // calculate term frequency (TF) for the current term in the document
+            // Calculate term frequency (TF) for the current term in the document
             float tf = static_cast<float>(pair.second) / terms.size();
 
-            try{
+            try {
                 indexer_db::IndexDocument indexDocument{document->url, tf, static_cast<int>(document->content.size())};
                 this->db.get()->upsertIndexDocument(indexDocument, term);
-            }catch(std::exception& e){
-                std::cerr << "Error exec upserting: " << e.what() << std::endl;
+            } catch (std::exception& e) {
+                std::cerr << "Error executing upsert: " << e.what() << std::endl;
             }
         }
     }
 
-
+    /**
+     * @brief Splits the content into unique terms based on a delimiter.
+     * 
+     * This function splits the content string into segments (terms) and counts the frequency of each unique term.
+     * 
+     * @param str The content string to be split.
+     * @param terms The map to store the terms and their frequencies.
+     * @param delimiter The character used to split the string.
+     */
     void Indexer::splitContentUniqueTerms(const std::string& str, std::unordered_map<std::string, int>& terms, char delimiter) {
         std::istringstream isstream(str);
         std::string segment;
@@ -39,6 +60,13 @@ namespace indexer {
         }
     }
 
+    /**
+     * @brief Removes whitespace from a string.
+     * 
+     * This function removes all whitespace characters from the given string.
+     * 
+     * @param str The string from which whitespace will be removed.
+     */
     void Indexer::removeWhitespace(std::string& str) {
         str.erase(std::remove_if(str.begin(), str.end(), ::isspace), str.end());
     }
